edc29ef595f788c9397d795fd847d9d944267561
Michael Kimpton <mike@sketchthat.com>
2018-01-17 20:17:06 +1100

    Working on fund transfering

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import * as crypto from 'crypto';
 import * as rp from 'request-promise';
 
 import { IAccountBalance } from './interfaces/accountBalance.interface';
+import { IFundTransferHistory } from './interfaces/fundTransferHistory.interface';
 import { IMarketOrderBook } from './interfaces/marketOrderBook.interface';
 import { IMarketTick } from './interfaces/marketTick.interface';
 import { IMarketTrade } from './interfaces/marketTrades.interface';
 import { ITradingFee } from './interfaces/tradingFee.interface';
+
+import { IPrivateRequest } from './interfaces/general/privateRequest.interface';
 
 export class BTCMarkets {
   private keys: {
@@ -22,20 +25,26 @@ export class BTCMarkets {
     this.accountFloat = 100000000;
   }
 
-  private async callPrivate(method: string): Promise<any> {
+  private async callPrivate(method: string, qs?: string): Promise<any> {
     const uri = `${this.base}${method}`;
     const hmac = this.generateHmac(method);
 
+
+
     try {
-      const opts = {
+      const opts: IPrivateRequest = {
         uri: uri,
         headers: {
           apiKey: this.keys.publicKey,
           timestamp: this.nonce,
           signature: hmac
         },
-        json: true
+        json: true,
       };
+
+      if (qs) {
+        opts.qs = qs;
+      }
 
       const resp = await rp.get(opts);
 
@@ -125,4 +134,50 @@ export class BTCMarkets {
 
     return resp;
   }
+
+  /*
+  private fundTransferPaging: {
+    newer: string;
+    older: string;
+  }
+
+  public async fundTransferHistory(newer?: boolean, older?: boolean): Promise<IFundTransferHistory> {
+    let qs = null;
+
+    if ((newer || older) && (this.fundTransferPaging.newer && this.fundTransferPaging.older)) {
+      let queryAddress;
+
+      if (newer) {
+        queryAddress = this.fundTransferPaging.newer;
+      } else if (older) {
+        queryAddress = this.fundTransferPaging.older;
+      }
+
+      if (queryAddress) {
+        const querySplit: string[] = queryAddress.split(/\?|&|=/);
+
+        if (querySplit && querySplit.length > 0) {
+          let o;
+          qs = {};
+
+          querySplit.forEach((query, i) => {
+            if (i > 0) {
+              if (i % 2) {
+                o = query;
+              } else {
+                qs[o] = query;
+              }
+            }
+          });
+        }
+      }
+    }
+
+    const resp: IFundTransferHistory = await this.callPrivate(`/fundtransfer/history`, qs);
+
+    this.fundTransferPaging = resp.paging;
+
+    return resp;
+  }
+  */
 }
